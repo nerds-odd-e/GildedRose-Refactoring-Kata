ceb8926d9a6077be673f335a8d8c1168bf38435b
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2021-05-05 20:06:38 +0800

    :recycle: use parameterized test for all normal tests

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,65 +1,37 @@
 package com.gildedrose;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class GildedRoseTest {
 
     private GildedRose app;
 
-    @Test
-    public void quality_reduce_one_per_day_when_not_expired() {
-        app = gildedRoseWithItem(normalItem(50, 1));
-
-        app.updateQuality();
-
-        assertItemQualityEquals(49);
+    public static Stream<Arguments> dataForNormalTests() {
+        return Stream.of(
+                arguments("quality_reduce_one_per_day_when_not_expired", 50, 1, 49),
+                arguments("quality_reduce_one_per_day_when_just_expired", 50, 0, 48),
+                arguments("quality_reduce_one_per_day_when_already_expired", 50, -1, 48),
+                arguments("quality_reduce_two_per_day_when_expired", 3, 0, 1),
+                arguments("quality_can_not_reduce_beyond_0_when_expired", 1, 0, 0),
+                arguments("quality_should_keep_as_0_even_when_not_expired", 0, 1, 0)
+        );
     }
 
-    @Test
-    public void quality_reduce_one_per_day_when_just_expired() {
-        app = gildedRoseWithItem(normalItem(50, 0));
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("dataForNormalTests")
+    public void normal_item(String testName, int currentQuality, int sellInDays, int expectedUpdateQuality) {
+        app = gildedRoseWithItem(normalItem(currentQuality, sellInDays));
 
         app.updateQuality();
 
-        assertItemQualityEquals(48);
-    }
-
-    @Test
-    public void quality_reduce_one_per_day_when_already_expired() {
-        app = gildedRoseWithItem(normalItem(50, -1));
-
-        app.updateQuality();
-
-        assertItemQualityEquals(48);
-    }
-
-    @Test
-    public void quality_reduce_two_per_day_when_expired() {
-        app = gildedRoseWithItem(normalItem(3, 0));
-
-        app.updateQuality();
-
-        assertItemQualityEquals(1);
-    }
-
-    @Test
-    public void quality_can_not_reduce_beyond_0_when_expired() {
-        app = gildedRoseWithItem(normalItem(1, 0));
-
-        app.updateQuality();
-
-        assertItemQualityEquals(0);
-    }
-
-    @Test
-    public void quality_should_keep_as_0_even_when_not_expired() {
-        app = gildedRoseWithItem(normalItem(0, 1));
-
-        app.updateQuality();
-
-        assertItemQualityEquals(0);
+        assertItemQualityEquals(expectedUpdateQuality);
     }
 
     private void assertItemQualityEquals(int expected) {
