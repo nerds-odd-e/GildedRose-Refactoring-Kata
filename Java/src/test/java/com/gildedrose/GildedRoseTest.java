950e4935784148d310d4d4b211bd120a4702ae99
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2021-05-05 18:44:40 +0800

    :recycle: refactor first three tests

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,28 +6,45 @@ import static org.junit.Assert.assertEquals;
 
 public class GildedRoseTest {
 
+    private GildedRose app;
+
     @Test
     public void quality_reduce_one_per_day_when_not_expired() {
-        Item[] items = new Item[]{new Item("normal", 1, 50)};
-        GildedRose app = new GildedRose(items);
+        app = gildedRoseWithItem(normalItem(50, 1));
+
         app.updateQuality();
-        assertEquals(49, app.items[0].quality);
+
+        assertItemQualityEquals(49);
     }
 
     @Test
     public void quality_reduce_one_per_day_when_just_expired() {
-        Item[] items = new Item[]{new Item("normal", 0, 50)};
-        GildedRose app = new GildedRose(items);
+        app = gildedRoseWithItem(normalItem(50, 0));
+
         app.updateQuality();
-        assertEquals(48, app.items[0].quality);
+
+        assertItemQualityEquals(48);
     }
 
     @Test
     public void quality_reduce_one_per_day_when_already_expired() {
-        Item[] items = new Item[]{new Item("normal", -1, 50)};
-        GildedRose app = new GildedRose(items);
+        app = gildedRoseWithItem(normalItem(50, -1));
+
         app.updateQuality();
-        assertEquals(48, app.items[0].quality);
+
+        assertItemQualityEquals(48);
+    }
+
+    private void assertItemQualityEquals(int expected) {
+        assertEquals(expected, app.items[0].quality);
+    }
+
+    private GildedRose gildedRoseWithItem(Item item) {
+        return new GildedRose(new Item[]{item});
+    }
+
+    private Item normalItem(int quality, int sellIn) {
+        return new Item("normal", sellIn, quality);
     }
 
 }
