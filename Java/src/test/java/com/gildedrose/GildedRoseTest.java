bfd547647a182937f7113f37ae78c767f527f634
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2021-05-05 21:19:05 +0800

    :white_check_mark: add first test for sell in days

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,5 +1,8 @@
 package com.gildedrose;
 
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -13,86 +16,102 @@ public class GildedRoseTest {
 
     private GildedRose app;
 
-    public static Stream<Arguments> dataForNormalTests() {
-        return Stream.of(
-                arguments("quality_reduce_one_per_day_when_not_expired", 50, 1, 49),
-                arguments("quality_reduce_one_per_day_when_just_expired", 50, 0, 48),
-                arguments("quality_reduce_one_per_day_when_already_expired", 50, -1, 48),
-                arguments("quality_reduce_two_per_day_when_expired", 3, 0, 1),
-                arguments("quality_can_not_reduce_beyond_0_when_expired", 1, 0, 0),
-                arguments("quality_should_keep_as_0_even_when_not_expired", 0, 1, 0)
-        );
-    }
-
-    @ParameterizedTest(name = "{0}")
-    @MethodSource("dataForNormalTests")
-    public void item(String testName, int currentQuality, int sellInDays, int expectedUpdateQuality) {
-        app = gildedRoseWithItem(item("normal", currentQuality, sellInDays));
+    @Test
+    public void sell_in_days_reduce_one_per_day_when_not_expired() {
+        app = gildedRoseWithItem(item("normal", 20, 1));
 
         app.updateQuality();
 
-        assertItemQualityEquals(expectedUpdateQuality);
+        assertEquals(0, app.items[0].sellIn);
     }
 
-    public static Stream<Arguments> dataForAgedBrieTests() {
-        return Stream.of(
-                arguments("quality increase one per day when not expired", 0, 1, 1),
-                arguments("quality increase two per day when just expired", 0, 0, 2),
-                arguments("quality increase two per day when already expired", 0, -1, 2),
-                arguments("quality can't exceed 50 when not expired", 50, 1, 50),
-                arguments("quality can't exceed 50 when expired", 49, 1, 50)
-        );
-    }
+    @Nested
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    public class Quality {
 
-    @ParameterizedTest(name = "{0}")
-    @MethodSource("dataForAgedBrieTests")
-    public void aged_brie_item(String testName, int currentQuality, int sellInDays, int expectedUpdateQuality) {
-        app = gildedRoseWithItem(item("Aged Brie", currentQuality, sellInDays));
+        public Stream<Arguments> dataForNormalTests() {
+            return Stream.of(
+                    arguments("quality_reduce_one_per_day_when_not_expired", 50, 1, 49),
+                    arguments("quality_reduce_one_per_day_when_just_expired", 50, 0, 48),
+                    arguments("quality_reduce_one_per_day_when_already_expired", 50, -1, 48),
+                    arguments("quality_reduce_two_per_day_when_expired", 3, 0, 1),
+                    arguments("quality_can_not_reduce_beyond_0_when_expired", 1, 0, 0),
+                    arguments("quality_should_keep_as_0_even_when_not_expired", 0, 1, 0)
+            );
+        }
 
-        app.updateQuality();
+        @ParameterizedTest(name = "{0}")
+        @MethodSource("dataForNormalTests")
+        public void normal_item(String testName, int currentQuality, int sellInDays, int expectedUpdateQuality) {
+            app = gildedRoseWithItem(item("normal", currentQuality, sellInDays));
 
-        assertItemQualityEquals(expectedUpdateQuality);
-    }
+            app.updateQuality();
 
-    public static Stream<Arguments> dataForSulfurasTests() {
-        return Stream.of(
-                arguments("quality is always 80 and never changes", 80, 1, 80)
-        );
-    }
+            assertItemQualityEquals(expectedUpdateQuality);
+        }
 
-    @ParameterizedTest(name = "{0}")
-    @MethodSource("dataForSulfurasTests")
-    public void sulfuras_item(String testName, int currentQuality, int sellInDays, int expectedUpdateQuality) {
-        app = gildedRoseWithItem(item("Sulfuras, Hand of Ragnaros", currentQuality, sellInDays));
+        public Stream<Arguments> dataForAgedBrieTests() {
+            return Stream.of(
+                    arguments("quality increase one per day when not expired", 0, 1, 1),
+                    arguments("quality increase two per day when just expired", 0, 0, 2),
+                    arguments("quality increase two per day when already expired", 0, -1, 2),
+                    arguments("quality can't exceed 50 when not expired", 50, 1, 50),
+                    arguments("quality can't exceed 50 when expired", 49, 1, 50)
+            );
+        }
 
-        app.updateQuality();
+        @ParameterizedTest(name = "{0}")
+        @MethodSource("dataForAgedBrieTests")
+        public void aged_brie_item(String testName, int currentQuality, int sellInDays, int expectedUpdateQuality) {
+            app = gildedRoseWithItem(item("Aged Brie", currentQuality, sellInDays));
 
-        assertItemQualityEquals(expectedUpdateQuality);
-    }
+            app.updateQuality();
 
-    public static Stream<Arguments> dataForBackstagePassesTests() {
-        return Stream.of(
-                arguments("quality increase one per day when sell in days > 10", 0, 11, 1),
-                arguments("quality increase two per day when sell in days = 10", 0, 10, 2),
-                arguments("quality increase two per day when sell in days < 10", 0, 9, 2),
-                arguments("quality increase two per day when sell in days > 5", 0, 6, 2),
-                arguments("quality increase three per day when sell in days = 5", 0, 5, 3),
-                arguments("quality increase three per day when sell in days < 5", 0, 4, 3),
-                arguments("quality increase three per day when sell in days < 5", 0, 4, 3),
-                arguments("quality drop to 0 when expired regardless quality value", 50, 0, 0),
-                arguments("quality can't exceed 50 when increasing two per day", 49, 9, 50),
-                arguments("quality can't exceed 50 when increasing three per day", 48, 4, 50)
-        );
-    }
+            assertItemQualityEquals(expectedUpdateQuality);
+        }
 
-    @ParameterizedTest(name = "{0}")
-    @MethodSource("dataForBackstagePassesTests")
-    public void backstage_passes_item(String testName, int currentQuality, int sellInDays, int expectedUpdateQuality) {
-        app = gildedRoseWithItem(item("Backstage passes to a TAFKAL80ETC concert", currentQuality, sellInDays));
+        public Stream<Arguments> dataForSulfurasTests() {
+            return Stream.of(
+                    arguments("quality is always 80 and never changes", 80, 1, 80)
+            );
+        }
 
-        app.updateQuality();
+        @ParameterizedTest(name = "{0}")
+        @MethodSource("dataForSulfurasTests")
+        public void sulfuras_item(String testName, int currentQuality, int sellInDays, int expectedUpdateQuality) {
+            app = gildedRoseWithItem(item("Sulfuras, Hand of Ragnaros", currentQuality, sellInDays));
 
-        assertItemQualityEquals(expectedUpdateQuality);
+            app.updateQuality();
+
+            assertItemQualityEquals(expectedUpdateQuality);
+        }
+
+
+        @ParameterizedTest(name = "{0}")
+        @MethodSource("dataForBackstagePassesTests")
+        public void backstage_passes_item(String testName, int currentQuality, int sellInDays, int expectedUpdateQuality) {
+            app = gildedRoseWithItem(item("Backstage passes to a TAFKAL80ETC concert", currentQuality, sellInDays));
+
+            app.updateQuality();
+
+            assertItemQualityEquals(expectedUpdateQuality);
+        }
+
+        Stream<Arguments> dataForBackstagePassesTests() {
+            return Stream.of(
+                    arguments("quality increase one per day when sell in days > 10", 0, 11, 1),
+                    arguments("quality increase two per day when sell in days = 10", 0, 10, 2),
+                    arguments("quality increase two per day when sell in days < 10", 0, 9, 2),
+                    arguments("quality increase two per day when sell in days > 5", 0, 6, 2),
+                    arguments("quality increase three per day when sell in days = 5", 0, 5, 3),
+                    arguments("quality increase three per day when sell in days < 5", 0, 4, 3),
+                    arguments("quality increase three per day when sell in days < 5", 0, 4, 3),
+                    arguments("quality drop to 0 when expired regardless quality value", 50, 0, 0),
+                    arguments("quality can't exceed 50 when increasing two per day", 49, 9, 50),
+                    arguments("quality can't exceed 50 when increasing three per day", 48, 4, 50)
+            );
+        }
+
     }
 
     private void assertItemQualityEquals(int expected) {
