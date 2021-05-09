e9dab4fd2d18ce25ee79e301656ab7895f1e68ad
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2021-05-09 15:13:41 +0800

    :recycle: lift up step 3 for Sulfuras simplify if block

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -48,41 +48,22 @@ class GildedRose {
             if (item.sellIn < 0) {
                 item.quality = 0;
             }
+        } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
         } else {
-            if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (item.quality > 0) {
+                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                    item.quality = item.quality - 1;
+                }
+            }
+
+            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                item.sellIn = item.sellIn - 1;
+            }
+
+            if (item.sellIn < 0) {
                 if (item.quality > 0) {
                     if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
                         item.quality = item.quality - 1;
-                    }
-                }
-
-                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    item.sellIn = item.sellIn - 1;
-                }
-
-                if (item.sellIn < 0) {
-                    if (item.quality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            item.quality = item.quality - 1;
-                        }
-                    }
-                }
-            } else {
-                if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.quality = item.quality - 1;
-                    }
-                }
-
-                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    item.sellIn = item.sellIn - 1;
-                }
-
-                if (item.sellIn < 0) {
-                    if (item.quality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            item.quality = item.quality - 1;
-                        }
                     }
                 }
             }
